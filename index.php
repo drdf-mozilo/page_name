e20ef12174ab4733e14fefd8b64ced3a79b5f439
David Ringsdorf <git@drdf.de>
2016-06-22 18:52:41 +0200

    add description, readme, licence

--- index.php
+++ index.php
@@ -7,19 +7,37 @@ if (! defined('IS_CMS')) {
  *
  * @author David Ringsdorf <git@drdf.de>
  * @copyright (c) 2016, David Ringsdorf
+ * @license The MIT License (MIT)
  */
 class page_name extends Plugin
 {
 
     const NAME = __CLASS__;
 
-    const VERSION = '0.1.0-alpha';
+    const VERSION = '0.2.0-beta';
 
     const AUTHOR = 'David Ringsdorf';
 
     const DOKU_URL = 'http://mozilo.davidringsdorf.de';
 
     const MOZILO_VERSION = '2.0';
+
+    private $_languageObject;
+
+    private $_pluginTemplateDir;
+
+    private $_licenceFile;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $languageFile = $this->PLUGIN_SELF_DIR . 'language' . DIRECTORY_SEPARATOR . $this->_fetchLanguageKey() . '.txt';
+
+        $this->_languageObject = new Language($languageFile);
+        $this->_pluginTemplateDir = $this->PLUGIN_SELF_DIR . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR;
+        $this->_licenceFile = $this->PLUGIN_SELF_DIR . DIRECTORY_SEPARATOR . 'licence.txt';
+    }
 
     /**
      *
@@ -85,7 +103,9 @@ class page_name extends Plugin
         $info[1] = self::MOZILO_VERSION;
 
         // Kurzbeschreibung.
-        $info[2] = '<code><span style="color:blue" >[ueber1</span><span style="color:red" >=page_name</span><span style="color:blue" >|</span>schöne Gärten<span style="color:blue" >]</span></code>';
+        $info[2] = $this->_template('admin_description', $this->_languageObject, [
+            'licence.text' => nl2br(file_get_contents($this->_licenceFile))
+        ]);
 
         // Name des Autors.
         $info[3] = self::AUTHOR;
@@ -95,6 +115,44 @@ class page_name extends Plugin
 
         // (optional) Platzhalter der im Seiten-Editor vorgeschlagen wird.
 
+        $info[5] = [
+            '[ueber1=page_name|...]' => $this->_languageObject->getLanguageValue('info.placeholder.title')
+        ];
+
         return $info;
+    }
+
+    /**
+     *
+     * @global Properties $ADMIN_CONF
+     * @global Properties $CMS_CONF
+     * @return string
+     */
+    private function _fetchLanguageKey()
+    {
+        global $ADMIN_CONF, $CMS_CONF;
+
+        $language = $CMS_CONF->get('cmslanguage');
+        if (IS_ADMIN) {
+            $language = $ADMIN_CONF->get('language');
+        }
+
+        return $language;
+    }
+
+    /**
+     *
+     * @param string $templateName
+     * @param Language $languageOnject
+     * @param array|NULL $param
+     * @return string
+     */
+    private function _template($templateName, Language $languageOnject = NULL, $param = NULL)
+    {
+        ob_start();
+        $t = $languageOnject;
+        $p = $param;
+        require $this->_pluginTemplateDir . $templateName . '.php';
+        return (string) ob_get_clean();
     }
 }
---
+++ template/admin_description.php
@@ -0,0 +1,25 @@
+<?php
+if (! defined('IS_CMS')) {
+    die();
+}
+
+/**
+ *
+ * @author David Ringsdorf <git@drdf.de>
+ * @copyright (c) 2016, David Ringsdorf
+ * @license The MIT License (MIT)
+ */
+?>
+<h1><?= $t->getLanguageValue('info.description.description_header'); ?></h1>
+<p><?= $t->getLanguageValue('info.description.description_text'); ?></p>
+<h2><?= $t->getLanguageValue('info.description.example_header'); ?></h2>
+<?= $t->getLanguageValue('info.description.example_text_1'); ?><br />
+<pre>
+  <code>
+    <span style="color: blue">[ueber1</span><span style="color: red">=page_name</span><span
+      style="color: blue">|</span><?= $t->getLanguageValue('info.description.example_text_2'); ?><span
+      style="color: blue">]</span>
+  </code>
+</pre>
+<h1><?= $t->getLanguageValue('info.description.licence_header'); ?></h1>
+<p><?= $p['licence.text'] ?></p>
